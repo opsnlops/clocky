edfbf23c3927de558e98b6be03d14fa3b6ef6035
April White <april@opsnlops.io>
2022-03-20 13:58:12 -0700

    Fade between hues for the seconds marker
    This is less distracting than changing the brightness! It also allows me
    to run it far faster, since I'm not stuck by the limits for how bright
    a pixel can be

--- src/seconds_ring.h
+++ src/seconds_ring.h
@@ -15,11 +15,11 @@ extern "C"
 #include "logging/logging.h"
 
 #define LED_RING_PIN 13
-#define MAX_BRIGHTNESS 50
 
 // This is 0.618033988749895 * (2**16)
 #define GOLDEN_RATIO_CONJUGATE 40503
 
 uint16_t getRandomHue();
+uint16_t interpolateHue(uint16_t oldHue, uint16_t newHue, uint8_t totalSteps, uint8_t currentStep);
 
 portTASK_FUNCTION_PROTO(secondRingTask, pvParameters);
